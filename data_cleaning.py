d108cf064f6d8eb0a7b19fd99999467ac314e43c
ChileMathAcc <chilese@outlook.com>
2024-10-14 12:59:25 +0100

    Adds a card_data cleaning method and an upload to local db method

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,10 +1,11 @@
 import data_extraction
 import pandas as pd
+import numpy as np
 
 
-class data_cleaning():
+class data_cleaner():
     '''
-    This takes data from data_exctration and cleans it in various ways.
+    This takes data from data_exctration and cleans it in one of three ways.
     1. Changes the data types of the columns
     2. Drops NULL values
     3. Removes duplicates
@@ -14,27 +15,31 @@ class data_cleaning():
     def __init__(self):
         clean_df = None
     
-    def clean_user_data(self):
+    def clean_user_data():
+        '''
+        Cleans the user data
+        '''
+        
         D = data_extraction.DatabaseConnector()         #Initialises from DatabaseConnector
         data = data_extraction.DatabaseExtractor.read_rds_table(DatabaseConnector = D,
                                                                 table = D.list_db_tables()[2])      #Retrieves data in panda format
-        prefered_col_types = {
-            'first_name': 'string',
-            'last_name': 'string',
-            'company': 'string',
-            'email_address': 'string',
-            'address': 'string',
-            'country': 'string',
-            'country_code': 'string',
-            'phone_number': 'string',
-            'user_uuid': 'string'
-        }
-        data = data.astype(prefered_col_types)      #Changes the data types of all the string-like columns
+        data = data.infer_objects()      #Changes the data types of all the string-like columns
         data['date_of_birth'] = pd.to_datetime(data['date_of_birth'], format = 'mixed', errors = 'coerce')      #Changes the data types of the datetime-like column
         data['join_date'] = pd.to_datetime(data['join_date'], format = 'mixed', errors = 'coerce')      #Changes the data types of the datetime-like column
         data = data.dropna(axis=0, how='any')       #Drops NULL values
         data = data.drop_duplicates()       #Drops duplicates
-        return data
+        return (data, 'user_data')
+    
+    def clean_card_data(link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"):
+        '''
+        Cleans the payment card data
+        '''
 
-print(data_cleaning().clean_user_data().dtypes)
-print(data_cleaning().clean_user_data().shape)
+        pdf_data = data_extraction.DatabaseExtractor.retrieve_pdf_data(link)
+        pdf_data['card_number'] = pdf_data['card_number'].str.replace('\D+','', regex = True)
+        pdf_data['card_number'] = pd.to_numeric(pdf_data['card_number'], errors = 'coerce', downcast = 'integer').convert_dtypes()      #Converts the card_numbers to int64
+        pdf_data['expiry_date'] = pd.to_datetime(pdf_data['expiry_date'], format = '%m/%y', errors = 'coerce')      #Converts the card expiry dates to datetime
+        pdf_data['date_payment_confirmed'] = pd.to_datetime(pdf_data['date_payment_confirmed'], format = 'mixed', errors = 'coerce')        #Converts payment date to datetime
+        pdf_data.dropna(axis = 0, how = 'any', inplace = True)          #Deletes NULL values from the dataframe
+        
+        return pdf_data
--- data_extraction.py
+++ data_extraction.py
@@ -1,65 +1,26 @@
-import yaml
-from sqlalchemy import create_engine
-from sqlalchemy import inspect
+from database_utils import DatabaseConnector
 import pandas as pd
 import tabula
 
-
-class DatabaseConnector():
-    '''
-    Establishes a connection to AWS for data retrieval 
-    '''
-    
-    def __init__(self):
-        self.engine = None
-        self.db_tables = []
-        
-    def read_db_creds(self):
-        '''
-        Opens a yaml document with the credentails for the database
-        '''
-        
-        with open('db_creds.yaml', 'r') as d:
-            db_creds = yaml.safe_load(d)
-        return db_creds
-    
-    
-    def init_db_engine(self):
-        '''
-        Uses the credentail from the above method to establish a connect to an RDB
-        '''
-        
-        db_creds = self.read_db_creds()
-        DATABASE_TYPE = 'postgresql'
-        DBAPI = 'psycopg2'
-        ENDPOINT = db_creds['RDS_HOST']
-        USER = db_creds['RDS_USER']
-        PASSWORD = db_creds['RDS_PASSWORD']
-        PORT = db_creds['RDS_PORT']
-        DATABASE = db_creds['RDS_DATABASE']
-        self.engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
-        return self.engine
-    
-    def list_db_tables(self):
-        '''
-        Retrieves a list of tables from an RDB
-        '''
-        
-        self.engine = self.init_db_engine()
-        inspector = inspect(self.engine)
-        self.db_tables = inspector.get_table_names()
-        return self.db_tables
-    
 
 class DatabaseExtractor():
     '''
     Uses pandas to create a dataframe from a table in the RDB
     '''
     
-    def read_rds_table(DatabaseConnector, table):
-        df = pd.read_sql_table(table, con = DatabaseConnector.engine, index_col = 0)
+    def read_rds_table(table : str):
+        '''
+        Retrieves a sql table from the RDB as a dataframe
+        '''
+        
+        df = pd.read_sql_table(table, con = DatabaseConnector.init_db_engine(), index_col = 0)
         return df
     
-    def retrieve_pdf_data(link):
-        pdf_data = pd.DataFrame(tabula.read_pdf(link))
+    def retrieve_pdf_data(self, link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"):
+        '''
+        Takes a link to a pdf-document and creates a dataframe of the tables in the pdf
+        '''
+        
+        pdf_data = tabula.read_pdf(link, pages = 'all', output_format = "dataframe")[0]     #Reads the tables off the pdf
         return pdf_data
+
